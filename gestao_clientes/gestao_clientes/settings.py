0978ee5980236585f56e6801202c279e35446f7e
aleleonel <aleleonel@gmail.com>
2018-12-21 11:56:05 -0200

    incluido bootstrapform in requirements-dev

--- gestao_clientes/gestao_clientes/settings.py
+++ gestao_clientes/gestao_clientes/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrapform',
     'clientes',
     'home',
 ]
@@ -131,3 +132,7 @@ LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = 'person_list'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    'statics',
+]
